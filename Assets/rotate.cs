31973dc3fcb2b27e520143cf9e1a1d9584386947
unknown <yuchen880401@gmail.com>
2021-08-24 00:09:50 +0800

    0823updated

---
+++ Assets/rotate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    float rotationleft = 360;
+    float rotationspeed = 20;
+    // Update is called once per frame
+    void Update()
+    {
+        float rotation = rotationspeed * Time.deltaTime;
+
+        if (rotationleft > rotation)
+        {
+            rotationleft -= rotation;
+        } else {
+            rotation = rotationleft;
+            rotationleft = 0;
+        }
+
+        transform.Rotate(0, 0, rotation);
+    }
+}
