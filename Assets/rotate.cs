fa11b9dc4657c1c310ac146682e25a601719b946
unknown <yuchen880401@gmail.com>
2021-09-11 16:03:33 +0800

    0911update

--- Assets/rotate.cs
+++ Assets/rotate.cs
@@ -11,19 +11,19 @@ public class rotate : MonoBehaviour
     }
 
     float rotationleft = 360;
-    float rotationspeed = 20;
+    float rotationspeed = 10;
     // Update is called once per frame
     void Update()
     {
         float rotation = rotationspeed * Time.deltaTime;
 
-        if (rotationleft > rotation)
-        {
-            rotationleft -= rotation;
-        } else {
-            rotation = rotationleft;
-            rotationleft = 0;
-        }
+        // if (rotationleft > rotation)
+        // {
+        //     rotationleft -= rotation;
+        // } else {
+        //     rotation = rotationleft;
+        //     rotationleft = 0;
+        // }
 
         transform.Rotate(0, 0, rotation);
     }
